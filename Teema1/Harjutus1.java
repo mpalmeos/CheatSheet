faafc1bd1fbba817a1a5d04513d7fd69a44a426a
mpalmeos <maie.palmeos@itcollege.ee>
2016-10-13 20:57:51 +0300

    Harjutused

---
+++ Teema1/Harjutus1.java
@@ -0,0 +1,19 @@
+package Teema1;
+
+/**
+ * Created by Maie on 13/10/2016.
+ */
+public class Harjutus1 {
+    public static void main(String[] args) {
+
+        for (int i = 0; i < 10; i++) {
+            for (int j = 0; j < 10; j++) {
+                System.out.printf("%4d", i + j);
+
+            }
+            System.out.println();
+        }
+
+        }
+
+        }
---
+++ Teema1/Harjutus2.java
@@ -0,0 +1,7 @@
+package Teema1;
+
+/**
+ * Created by Maie on 13/10/2016.
+ */
+public class Harjutus2 {
+}
